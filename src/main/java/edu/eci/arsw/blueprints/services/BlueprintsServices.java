69f9ffa02dc4ebecb1126b72bee41b786566c90f
ana.silva <ana.silva-b@escuelaing.edu.co>
2021-10-06 21:58:02 -0500

    Delete

--- src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -27,54 +27,27 @@ public class BlueprintsServices {
     @Autowired
     BlueprintsPersistence bpp = null;
 
-    
-    /**
-     * Metodo encargado de adicionar un nuevo BluePrint
-     * @param bp
-     * @throws BlueprintPersistenceException 
-     */
     public void addNewBlueprint(Blueprint bp) throws BlueprintPersistenceException{
         bpp.saveBlueprint(bp);
     }
-    
-        /**
-     * Metodo encargado de modificar o adicionar un nuevo BluePrint
-     * @param bp
-     * @param author
-     * @param name
-     * @throws BlueprintPersistenceException 
-     */
     public void modifyOrAddBlueprint(Blueprint bp, String author, String name) throws BlueprintPersistenceException{
         bpp.modifyOrAddBlueprintS(bp, author, name);
     }
-    /**
-     * Metodo encargado de traer todos los BluePrints
-     * @return Blueprint
-     * @throws BlueprintNotFoundException 
-     */
     public Set<Blueprint> getAllBlueprints() throws BlueprintNotFoundException{
         return bpp.getAllBluePrint();
     }
     
-    /**
-     * 
-     * @param author blueprint's author
-     * @param name blueprint's name
-     * @return the blueprint of the given name created by the given author
-     * @throws BlueprintNotFoundException if there is no such blueprint
-     */
     public Blueprint getBlueprint(String author,String name) throws BlueprintNotFoundException{
         return bpp.getBlueprint(author , name);
     }
     
-    /**
-     * 
-     * @param author blueprint's author
-     * @return all the blueprints of the given author
-     * @throws BlueprintNotFoundException if the given author doesn't exist
-     */
     public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
         return bpp.getBlueprintByAuthor(author);
     }
+
+    public void deleteBlueprint(String author, String name) throws BlueprintNotFoundException {
+        bpp.deleteBlueprint(author, name);
+    }
+
 
 }
